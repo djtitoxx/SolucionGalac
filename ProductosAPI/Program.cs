b0660889c5ad568515de8f2d0d0f0113098f6579
Carlos.Garcia <carlos.garcia@galac.com>
2022-03-31 13:59:33 -0400

    Commit con comentarios

--- ProductosAPI/Program.cs
+++ ProductosAPI/Program.cs
@@ -24,3 +24,26 @@ namespace ProductosAPI
                 });
     }
 }
+
+
+
+/*
+Errores:
+
+200: Todo fue correcto y la petición se hizo sin problemas.
+
+201: Informa que un elemento fue creado o modificado en el Servidor!
+
+400: Hubo algun error en la petocion y algo salio mal
+
+401: Desautorizado, el cliente no tiene permisos de entrar a nuestro server o de hacer peticiones a nuestro server.
+
+403: que nuestro server ha entendido nuestra peticion, pero se niega a utilizarla.
+
+405: Metodo no permitido, es decir, que el server tiene bloqueado algun metodo http (GET, POST, DELETE, PUT, PATCH, etc)
+
+500: Que algo anda mal con nuestro server a nivel interno, sea algun error en el codigo o alguna otra cosa ajena a nuestra logica.
+
+ 
+ 
+ */
